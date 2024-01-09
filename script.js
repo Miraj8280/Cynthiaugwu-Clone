60564e5e67a3f9dd07cc475ad5f7cc74c3148cc4
Miraj Asraf <mirajasraf786@gmail.com>
2024-01-09 22:45:52 +0530

    Mini circle mouse movement added

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function circleMouseFollower() {
+    window.addEventListener("mousemove", function(details){
+        // console.log(details);
+        // console.log(details.clientX, details.clientY);
+
+        document.querySelector("#minicircle").style.transform = `translate(${details.clientX}px, ${details.clientY}px)`;
+    })
+}
+
+circleMouseFollower();
