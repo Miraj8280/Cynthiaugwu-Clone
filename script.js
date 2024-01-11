6efbd21acfbaee07c6705723edcbb70b8023ced9
Miraj Asraf <mirajasraf786@gmail.com>
2024-01-11 10:44:23 +0530

    Animated the first page

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+  el: document.querySelector("#main"),
+  smooth: true,
 });
 
-function circleMouseFollower() {
-    window.addEventListener("mousemove", function(details){
-        // console.log(details);
-        // console.log(details.clientX, details.clientY);
+function firstPageAnimation() {
+  let timeLine = gsap.timeline();
 
-        document.querySelector("#minicircle").style.transform = `translate(${details.clientX}px, ${details.clientY}px)`;
-    })
+  timeLine.from("#nav", {
+    y: -10,
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut
+  })
+  .to(".boundingelem", {
+    y: 0,
+    ease: Expo.easeInOut,
+    duration: 2,
+    delay: -1,
+    stagger: .2
+  })
+  .from("#homefooter", {
+    y: -10,
+    opacity: 0,
+    duration: 1.5,
+    delay: -1,
+    ease: Expo.easeInOut
+  })
+}
+
+function circleMouseFollower() {
+  window.addEventListener("mousemove", function (details) {
+    // console.log(details);
+    // console.log(details.clientX, details.clientY);
+    document.querySelector(
+      "#minicircle"
+    ).style.transform = `translate(${details.clientX}px, ${details.clientY}px)`;
+  });
 }
 
 circleMouseFollower();
+firstPageAnimation();
